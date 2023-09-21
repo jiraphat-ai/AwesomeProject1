99545d34d80735990be89707cb7d1d60edd771df
Your Name <you@example.com>
2023-09-22 00:42:33 +0700

    หน้าhome

--- App.js
+++ App.js
@@ -46,6 +46,25 @@ function App() {
           }}
         />
 
+<Stack.Screen name="Add password" component={Add_password}
+        
+        options={{
+          title: 'My Password',
+          headerStyle: {
+            backgroundColor: '#369EFF',
+          },
+          headerTintColor: '#000',
+          headerTitleAlign: 'center', // จัดตำแหน่งให้ข้อความอยู่ตรงกลา
+          headerTitleStyle: {
+          },
+          footterStyle: {
+            backgroundColor: '#369EFF',
+          },
+
+        }}
+      />
+
+
         <Stack.Screen name="Delete" component={Delete}
         
         options={{
@@ -124,17 +143,17 @@ function CustomNavigationBar({ navigation, back, route }) {
         <Text style={{ textAlign: 'center' , color: 'black', backgroundColor: 'black', fontWeight: 'bold' , padding: 10 }}>MENU</Text>
         </Menu.Item>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Icon name="th" size={20} color="black" />
+        <Icon name="th" size={20} color="black" paddingLeft={10}/>
         <Menu.Item onPress={() => navigation.navigate('all_item')} title="All item"
         style={{ paddingLeft: 10 }} // เพิ่มช่องว่างด้านซ้ายของ Text
         /></View>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Icon name="star" size={20} color="black" />
+        <Icon name="star" size={20} color="black" paddingLeft={10}/>
         <Menu.Item onPress={() => console.log('Option 5 was pressed')} title="Favorites"
         style={{ paddingLeft: 10 }} // เพิ่มช่องว่างด้านซ้ายของ Text
         /></View>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Icon name="trash-o" size={20} color="black" />
+        <Icon name="trash-o" size={20} color="black" paddingLeft={10}/>
         <Menu.Item onPress={() => navigation.navigate('Delete')} title="Delete"
         style={{ paddingLeft: 10 }} // เพิ่มช่องว่างด้านซ้ายของ Text
         /></View>
@@ -143,17 +162,17 @@ function CustomNavigationBar({ navigation, back, route }) {
         <Text style={{ color: 'black', backgroundColor: 'black', padding: 10 }}>Types</Text>
         </Menu.Item>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Icon name="credit-card-alt" size={20} color="black" />
+        <Icon name="credit-card-alt" size={20} color="black" paddingLeft={10}/>
         <Menu.Item onPress={() => console.log('Option 5 was pressed')} title="Card"
         style={{ paddingLeft: 3 }} // เพิ่มช่องว่างด้านซ้ายของ Text
         /></View>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Icon name="arrow-circle-o-right" size={20} color="black" />
+        <Icon name="arrow-circle-o-right" size={20} color="black" paddingLeft={10}/>
         <Menu.Item onPress={() => console.log('Option 5 was pressed')} title="Login"
         style={{ paddingLeft: 11 }} // เพิ่มช่องว่างด้านซ้ายของ Text
         /></View>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Icon name="check-square-o" size={20} color="black" />
+        <Icon name="check-square-o" size={20} color="black" paddingLeft={10} />
         <Menu.Item onPress={() => console.log('Option 5 was pressed')} title="Identity"
         style={{ paddingLeft: 11 }} // เพิ่มช่องว่างด้านซ้ายของ Text
         /></View>
@@ -163,7 +182,7 @@ function CustomNavigationBar({ navigation, back, route }) {
         <Text style={{ color: 'black', backgroundColor: 'black', padding: 10 }}></Text>
         </Menu.Item>
         <View style={{ flexDirection: 'row', alignItems: 'center' }}>
-        <Icon name="folder" size={20} color="black" />
+        <Icon name="folder" size={20} color="black" paddingLeft={10} />
         <Menu.Item onPress={() => console.log('Option 5 was pressed')} title="No Folders"
         style={{ paddingLeft: 11 }} // เพิ่มช่องว่างด้านซ้ายของ Text
         /></View>
--- screens/Home.js
+++ screens/Home.js
@@ -1,13 +1,90 @@
+import React, { useState } from 'react';
+import { StyleSheet, View, TextInput, Text, FlatList } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import { Button } from 'react-native-paper';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Add_password from './add_password';
 
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+export default function Home({ navigation }) {
+  const [headerText, setHeaderText] = useState('');
+  const [items, setItems] = useState([]);
+  const Stack = createNativeStackNavigator();
 
-export default class Home extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
+  const handleAddItem = () => {
+    if (headerText) {
+      setItems([...items, headerText]);
+      setHeaderText('');
+    }
+  };
+
+  return (
+    <View style={styles.container}>
+      <View style={StyleSheet.flatten([{ marginTop: -5 }, styles.header])}>
+        <Icon name="search" size={24} color="black" style={styles.icon} />
+        <TextInput
+          style={styles.headerText}
+          value={headerText}
+          onChangeText={(text) => setHeaderText(text)}
+          placeholder="Search..."
+          placeholderTextColor="white"
+        />
       </View>
-    )
-  }
+      <FlatList
+        data={items}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <View style={styles.item}>
+            <Text style={styles.itemText}>{item}</Text>
+          </View>
+        )}
+      />
+
+
+      <Button style={{ backgroundColor: '#0483F8', justifyContent: 'center', marginTop: 100, marginHorizontal: 160 }} icon="plus" mode="contained" onPress={() => navigation.navigate('Add password')} title="add item">
+        add item
+      </Button>
+    </View>
+
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+
+    justifyContent: 'flex-start',
+    paddingTop: 10,
+  },
+  header: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    backgroundColor: '#369EFF',
+    padding: 10,
+    borderRadius: 50,
+  },
+  headerText: {
+    fontSize: 18,
+    marginLeft: 20,
+    color: 'white',
+    flex: 1,
+    backgroundColor: 'transparent',
+  },
+  addButtonText: {
+    color: 'white',
+    fontWeight: 'bold',
+  },
+  icon: {
+    backgroundColor: 'white',
+    padding: 8,
+    borderRadius: 24,
+  },
+  item: {
+    backgroundColor: 'lightgray',
+    marginVertical: 8,
+    padding: 16,
+    borderRadius: 8,
+  },
+  itemText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+});
