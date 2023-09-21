63ec5d7366edfd1f8809e0b09670d6930c8e6e7a
ballalone1234 <ballpol.127@gmail.com>
2023-09-21 22:38:36 +0700

    Vi

--- screens/View_item.js
+++ screens/View_item.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import { FAB } from 'react-native-paper';
+import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
 
-export default class View_item extends Component {
-  render() {
-    return (
-      <View>
-        <Text> textInComponent </Text>
-      </View>
-    )
-  }
+
+
+function View_item() {
+  const [number, onChangeNumber] = React.useState('');
+    var payments = [];
+    for(let i = 0; i < 2; i++){
+  
+      payments.push(
+        <View key = {i}>
+           <TextInput
+        style={styles.input}
+        onChangeText={onChangeNumber}
+        value={number}
+        placeholder="useless placeholder"
+        keyboardType="numeric"
+      />
+        </View>
+      )
+    }
+  return ( <View>
+    <View>
+      <View><Text>No</Text></View>
+      <View><Text>Name</Text></View>
+      <View><Text>Preference</Text></View>
+    </View>
+
+    { payments }
+  </View>)
 }
+const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
+export default View_item;
+
