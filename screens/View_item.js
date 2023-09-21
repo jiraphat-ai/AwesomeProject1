06df343c33e336902a39f2c28598f3ef8e4d63b0
ballalone1234 <ballpol.127@gmail.com>
2023-09-22 00:41:29 +0700

    all item edit

--- screens/View_item.js
+++ screens/View_item.js
@@ -1,43 +1,56 @@
 import React, { Component } from 'react'
 import { FAB } from 'react-native-paper';
-import { View, Text, TextInput, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { Dimensions } from 'react-native';
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
+function View_item({ navigation, route }) {
+  const [text, setText] = React.useState("");
+  const [passwordVisible, setPasswordVisible] = React.useState(true);
+  const screen_width = Dimensions.get('screen').width;
+  var input_label = [{
+    label: 'Name',
+    icon: <Icon style={{ padding:15, justifyContent: 'center' }} name="eye" size={20} color="black" />
+  }, {label:'Password',icon: <Icon style={{ padding:15, justifyContent: 'center' }} name="eye" size={20} color="black" />}, {label:'Url'}];
+  var inputs = [];
+  for (let i = 0; i < input_label.length; i++) {
 
+    inputs.push(
+      <View style={styles.input} key={i}>
+        <TextInput style={{ width: screen_width * 0.7 }}
+          label={input_label[i].label}
+          secureTextEntry={passwordVisible}
+          value={text}
+          right={<TextInput.Icon name={passwordVisible ? "eye" : "eye-off"} onPress={() => setPasswordVisible(!passwordVisible)} />}
+          onChangeText={text => setText(text)
+          
+          }
 
-function View_item() {
-  const [number, onChangeNumber] = React.useState('');
-    var payments = [];
-    for(let i = 0; i < 2; i++){
-  
-      payments.push(
-        <View key = {i}>
-           <TextInput
-        style={styles.input}
-        onChangeText={onChangeNumber}
-        value={number}
-        placeholder="useless placeholder"
-        keyboardType="numeric"
-      />
-        </View>
-      )
-    }
-  return ( <View>
-    <View>
-      <View><Text>No</Text></View>
-      <View><Text>Name</Text></View>
-      <View><Text>Preference</Text></View>
+        />
+      {input_label[i]?.icon}
+      </View>
+    )
+  }
+  return (<View>
+
+    <View style={styles.contriner}>
+      <View><Text>{route.params?.type}</Text></View>
     </View>
 
-    { payments }
+    {inputs}
   </View>)
 }
 const styles = StyleSheet.create({
   input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
+    paddingHorizontal: 20,
+    paddingVertical: 10,
+    flexDirection: 'row',
+    width: 100
   },
+  contriner: {
+    padding: 20
+  }
 });
 export default View_item;
 
